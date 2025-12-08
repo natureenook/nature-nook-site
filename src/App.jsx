dc3edbb144bfe1aa121b78fcf59f753ce8da2de3
natureenook <naaturee.nook@gmail.com>
2025-12-08 23:25:01 +0400

    Add/update components and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Components/Header'
-import VideoPlayer from './Components/Video'
+import Header from './Header'
+import VideoPlayer from './Video'
 
 
 function App() {
